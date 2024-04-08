0df6de380d9ce85a005f77fe87f26aac9bd923f4
Your Name <you@example.com>
2024-04-07 22:40:59 -0500

    nuevos cambios

--- app/page.js
+++ app/page.js
@@ -1,26 +1,25 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
-
 const Page = () => (
   <div className="bg-gradient-to-r from-cyan-900 to-blue-900 overflow-hidden">
     <Navbar />
     <Hero />
     <div className="relative">
-    <About />
+      <About />
       <div className="gradient-03 z-0" />
-    <Explore />
+      <Explore />
     </div>
     <div className="relative">
-    <GetStarted />
-    <div className="gradient-04 z-0" />
-    <WhatsNew />
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
     <World />
     <div className="relative"> 
-    <Insights />
-    <div className="gradient-04 z-0" />
-    <Feedback className="gradient-02 z-0" />
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback className="gradient-02 z-0" />
     </div>
     <Footer />
   </div>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,8 @@
-'use client';
-
 import React from 'react';
-import { motion } from 'framer-motion'; // Importar motion desde framer-motion
-import styles from '../styles';
+import { motion } from 'framer-motion';
 import { socials } from '../constants';
 import { footerVariants } from '../utils/motion';
+import styles from '../styles';
 
 const Footer = () => (
   <motion.footer
@@ -17,22 +15,22 @@ const Footer = () => (
 
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[64px] text-[44px] text-white">Conoce Mas Nuestro Proyecto</h4>
+        <h4 className="font-bold md:text-64px text-44px text-white">Conoce Más Nuestro Proyecto</h4>
         <button
           type="button"
-          className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[26px] gap-[12px]"
+          className="flex items-center h-fit py-4 px-6 bg-#25618b rounded-26px gap-12px"
         >
-          <img src="/headset.svg" alt="headset" className="w-[24px] h-[24px] object-contain" />
-          <span className="font-normal text-[16px] text-white">Contáctanos</span>
+          <img src="/headset.svg" alt="headset" className="w-24px h-24px object-contain" />
+          <span className="font-normal text-16px text-white">Contáctanos</span>
         </button>
       </div>
 
       <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="mb-50px h-2px bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">ACA BALCONES</h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
+          <h4 className="font-extrabold text-24px text-white">ACA BALCONES</h4>
+          <p className="font-normal text-14px text-white opacity-50">
             Copyright © 2024 ACA-BALCONES. Todos los derechos reservados.
           </p>
           <div className="flex gap-4">
@@ -41,7 +39,7 @@ const Footer = () => (
                 key={social.name}
                 src={social.url}
                 alt={social.name}
-                className="w-[24px] h-[24px] object-contain cursor-pointer"
+                className="w-24px h-24px object-contain cursor-pointer"
               />
             ))}
           </div>
