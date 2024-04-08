5272374c8363c07bb5c48575b93121f3dfd4bfa8
Your Name <you@example.com>
2024-04-08 13:47:56 -0500

    head

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>Academy - Balcones </title>
+    <title>Academy- Balcones </title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/aca logo.png" />
   </>
